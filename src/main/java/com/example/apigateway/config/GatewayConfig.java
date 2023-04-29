162676cde39a4a9950deb17d46ee033992ae8158
Roberto RW <robertorw7@gmail.com>
2023-04-29 12:59:36 -0700

    first commit

---
+++ src/main/java/com/example/apigateway/config/GatewayConfig.java
@@ -0,0 +1,27 @@
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.util.Arrays;
+
+
+@Configuration
+public class GatewayConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+
+}
